739af0e11e0aa9ce2ff0f03e16f349606d495e3e
Lucas Soares <lucassoares@example.com>
2026-04-13 19:43:12 -0300

    chore: flatten project structure to root for better Lovable compatibility

--- remix-of-agency-hub/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
-// Force update for Lovable: Mon Apr 13 19:42:28 -03 2026
